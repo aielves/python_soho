d910a789cc3d982d34200308c2ce0521df4035b2
chenjianqiang <chenjianqiang@192.168.1.251>
2018-02-07 17:51:37 +0800

    增肌AES加密解密工具类实现(尚未处理中文加密失败问题)

--- utils/AES.py
+++ utils/AES.py
@@ -1,25 +1,50 @@
 from Crypto.Cipher import AES
 
-key = b'abcdefgh'
+UTF8 = "UTF-8";
 
-def pad(text):
-    while len(text) % 16 != 0:
-        text += b' '
-    print(text)
-    return text
+key = "N6(lWQGN#O+OQ5ev"
+
+
+def pad_text(encrypted_text):
+    while len(encrypted_text) % 16 != 0:
+        encrypted_text += "\0"
+    print(encrypted_text)
+    return encrypted_text
 
 
 # 加密秘钥需要长达16位字符，所以进行空格拼接
 def pad_key(key):
     while len(key) % 16 != 0:
-        key += b' '
+        key += "\0"
     print(key)
     return key
 
 
-aes = AES.new(pad_key(key), AES.MODE_ECB)
-text = b'woshijiamineirong'
-encrypted_text = aes.encrypt(pad(text))
-print(encrypted_text)
-encrypted_text = str(aes.decrypt(encrypted_text))
-print(encrypted_text)
+def decrypt(encrypted_text, encrypted_key):
+    # 加密后hex字符串转成字节
+    encrypted_text = bytes.fromhex(encrypted_text)
+    # 密钥字符串转成字节并按16位补全
+    encrypted_key = bytes(pad_key(encrypted_key), encoding=UTF8)
+    aes = AES.new(encrypted_key, AES.MODE_ECB)
+    encrypted_text = aes.decrypt(encrypted_text)
+    encrypted_text = str(encrypted_text, encoding=UTF8).replace("\0", "")
+    return encrypted_text
+
+
+def encrypt(encrypted_text, encrypted_key):
+    # 加密字符串转成字节并按16位补全
+    encrypted_text = bytes(pad_text(encrypted_text), encoding=UTF8)
+    # 密钥字符串转成字节并按16位补全
+    encrypted_key = bytes(pad_key(encrypted_key), encoding=UTF8)
+    aes = AES.new(encrypted_key, AES.MODE_ECB)
+    encrypted_text = aes.encrypt(encrypted_text)
+    # 加密后字节转成hex字符串
+    encrypted_text = ''.join(["%02X" % x for x in encrypted_text]).strip().lower()
+    return encrypted_text
+
+
+if __name__ == "__main__":
+    s = encrypt("123_hello world!", key)
+    print(s)
+    s = decrypt(s, key)
+    print(s)
--- utils/MD5.py
+++ utils/MD5.py
@@ -1,10 +1,7 @@
 import hashlib
-from binascii import b2a_hex, a2b_hex
-
-from Crypto.Cipher import AES
 
 
-def md5(*args):
+def encrypt(*args):
     # 创建md5对象
     md5 = hashlib.md5()
     text = args[0]
@@ -14,30 +11,8 @@ def md5(*args):
     return str(md5.hexdigest()).lower()
 
 
-# 加密函数，如果text不是16的倍数【加密文本text必须为16的倍数！】，那就补足为16的倍数
-def aes_in(text, key):
-    cryptor = AES.new(key, AES.MODE_CBC, key)
-    # 密钥key 长度必须为16（AES-128）、24（AES-192）、或32（AES-256）Bytes 长度.目前AES-128足够用
-    length = 16
-    count = len(text)
-    if (count % length != 0):
-        add = length - (count % length)
-    else:
-        add = 0
-        text = text + ('\0' * add)
-        text = cryptor.encrypt(text)
-        return b2a_hex(text)
-
-
-def decrypt(self, text):
-    cryptor = AES.new(self.key, self.mode, self.key)
-    plain_text = cryptor.decrypt(a2b_hex(text))
-    return plain_text.rstrip('\0')
-
-
 if (__name__ == "__main__"):
     s = "test"
     print('MD5加密前为 ：' + s)
-    print('MD5加密后为 ：' + md5(s))
-    print('MD5加密后为 ：' + md5(s, "cP1of$@T"))
-    print(aes_in("test", "nAjECibDL2vfTsoI"))
+    print('MD5加密后为 ：' + encrypt(s))
+    print('MD5加密后为 ：' + encrypt(s, "cP1of$@T"))
